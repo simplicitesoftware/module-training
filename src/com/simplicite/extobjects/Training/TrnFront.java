f51da1420f2bfddd17912ee9a731daccf9dbe67e
designer (Designer) <no-reply@simplicite.fr>
2022-09-16 11:22:18 +0200

    improved ui translations

--- src/com/simplicite/extobjects/Training/TrnFront.java
+++ src/com/simplicite/extobjects/Training/TrnFront.java
@@ -3,11 +3,27 @@ package com.simplicite.extobjects.Training;
 import java.util.*;
 import com.simplicite.util.*;
 import com.simplicite.util.tools.*;
+import java.io.IOException;
 
 /**
  * External object TrnFront
  */
 public class TrnFront extends com.simplicite.webapp.web.StaticSiteExternalObject {
 	private static final long serialVersionUID = 1L;
+	private static final String NOT_FOUND = "notfound.html";
 
+	@Override
+	public Object notfound(Parameters params)
+	{
+		AppLog.info("Not found on: " + params.getBaseLocation(), getGrant());
+		try {
+			if (NOT_FOUND.equals(params.getURI())) // Avoid infinite loop
+				throw new IOException();
+
+			return displayFile(NOT_FOUND);
+		} catch (IOException e) {
+			AppLog.error(null, e, getGrant());
+			return super.notfound(params);
+		}
+	}
 }
