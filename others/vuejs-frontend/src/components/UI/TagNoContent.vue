cc3371480a5c391b79ff6c5f8b42de8b50490e47
gaubert <germain40k@hotmail.fr>
2023-02-03 15:31:31 +0100

    fixed router bug

--- others/vuejs-frontend/src/components/UI/TagNoContent.vue
+++ others/vuejs-frontend/src/components/UI/TagNoContent.vue
@@ -5,8 +5,26 @@
 </template>
 
 <script>
+  import {mapState, mapGetters} from 'vuex';
   export default {
+    
+
     name: 'TagNoContent',
+    computed: {
+      ...mapState({
+        tree: state => state.tree.tree,
+      }),
+      ...mapGetters({
+        isSortedByTag: "ui/isSortedByTag",
+      })
+    },
+    beforeMount() {
+      // before component rendering, check if there is there is no content and tags selected. if true then go back to home page
+      if(this.tree.length === 0 && !this.isSortedByTag) {
+        this.$router.push("/");
+      }
+    }
+    
   }
 </script>
 
