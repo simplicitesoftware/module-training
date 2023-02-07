72f40aabba47b1c8c367580ac36eaeb320592020
designer (Designer) <no-reply@simplicite.fr>
2023-02-07 11:16:27 +0100

    add TrnSyncException

---
+++ src/com/simplicite/commons/Training/TrnSyncException.java
@@ -0,0 +1,16 @@
+package com.simplicite.commons.Training;
+
+import java.util.*;
+
+import com.simplicite.util.*;
+import com.simplicite.util.exceptions.*;
+import com.simplicite.util.tools.*;
+
+/**
+ * Shared code TrnSyncException
+ */
+public class TrnSyncException implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+
+	
+}
