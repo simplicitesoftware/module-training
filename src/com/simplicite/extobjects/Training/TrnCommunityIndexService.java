d74b105b2c69fe19226ded2b93202614898024e6
gaubert <germain40k@hotmail.fr>
2023-06-20 11:42:00 +0200

    add post in community index service

--- src/com/simplicite/extobjects/Training/TrnCommunityIndexService.java
+++ src/com/simplicite/extobjects/Training/TrnCommunityIndexService.java
@@ -1,9 +1,7 @@
 package com.simplicite.extobjects.Training;
 
-import java.util.*;
 
 import com.simplicite.util.*;
-import com.simplicite.util.exceptions.*;
 import com.simplicite.util.tools.*;
 
 /**
@@ -12,4 +10,16 @@ import com.simplicite.util.tools.*;
 public class TrnCommunityIndexService extends com.simplicite.webapp.services.RESTServiceExternalObject {
 	private static final long serialVersionUID = 1L;
 
+    @Override
+	public Object post(Parameters params) {
+		try {
+            AppLog.info("TrnCommunityIndexService " + params.toString(), getGrant());
+			setHTTPStatus(200);
+			return "OK";
+		} catch (Exception e) {
+			AppLog.error(getClass(), "sync", e.getMessage(), e, Grant.getSystemAdmin());
+			setHTTPStatus(500);
+			return e.getMessage();
+		}
+	}
 }
