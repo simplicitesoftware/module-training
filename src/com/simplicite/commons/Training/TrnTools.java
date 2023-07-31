e986ce0800cd8c1550dee060e046c942d458b83b
gaubert <germain40k@hotmail.fr>
2023-07-31 14:47:29 +0200

    add support for https and ssl git checkout

--- src/com/simplicite/commons/Training/TrnTools.java
+++ src/com/simplicite/commons/Training/TrnTools.java
@@ -3,10 +3,13 @@ package com.simplicite.commons.Training;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 
+import com.simplicite.util.AppLog;
 import com.simplicite.util.Grant;
 import com.simplicite.util.tools.SyntaxTool;
 
@@ -14,135 +17,152 @@ import com.simplicite.util.tools.SyntaxTool;
  * Shared code TrnTools
  */
 public class TrnTools implements java.io.Serializable {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+	private static final Pattern SSL_URL_PATTERN = Pattern.compile("^[a-zA-Z\\d]+@[a-zA-Z\\.:\\/\\d]+$");
 
-  public static String toSnake(String str) {
-    return SyntaxTool.forceCase(StringUtils.stripAccents(str), SyntaxTool.SNAKE, true);
-  }
+	public static String toSnake(String str) {
+		return SyntaxTool.forceCase(StringUtils.stripAccents(str), SyntaxTool.SNAKE, true);
+	}
 
-  public static String path2Front(String path) {
-    return path.replaceAll("(CTG|LSN)_[0-9]+_", "");
-  }
+	public static String path2Front(String path) {
+		return path.replaceAll("(CTG|LSN)_[0-9]+_", "");
+	}
 
-  public static boolean isUiMode() throws TrnConfigException {
-    try {
-      return "UI".equals(getContentEdition().optString("mode"));
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	public static boolean isUiMode() throws TrnConfigException {
+		try {
+			return "UI".equals(getContentEdition().optString("mode"));
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  public static boolean isFileSystemMode() throws TrnConfigException {
-    try {
-      return "FILESYSTEM".equals(getContentEdition().optString("mode"));
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	public static boolean isFileSystemMode() throws TrnConfigException {
+		try {
+			return "FILESYSTEM".equals(getContentEdition().optString("mode"));
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  public static boolean isElasticSearchMode() throws TrnConfigException {
-    try {
-      return "elasticsearch".equals(getIndexEngine());
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	public static boolean isElasticSearchMode() throws TrnConfigException {
+		try {
+			return "elasticsearch".equals(getIndexEngine());
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  public static boolean isSimpliciteMode() throws TrnConfigException {
-    try {
-      return "simplicite".equals(getIndexEngine());
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	public static boolean isSimpliciteMode() throws TrnConfigException {
+		try {
+			return "simplicite".equals(getIndexEngine());
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  private static JSONObject getTrnConfig() throws TrnConfigException {
-    try {
-      return new JSONObject(Grant.getSystemAdmin().getParameter("TRN_CONFIG"));
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	private static JSONObject getTrnConfig() throws TrnConfigException {
+		try {
+			return new JSONObject(Grant.getSystemAdmin().getParameter("TRN_CONFIG"));
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  public static JSONObject getContentEdition() throws TrnConfigException {
-    try {
-      return getTrnConfig().getJSONObject("content_edition");
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	public static JSONObject getContentEdition() throws TrnConfigException {
+		try {
+			return getTrnConfig().getJSONObject("content_edition");
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  private static JSONObject getContentIndexation() throws TrnConfigException {
-    try {
-      return getTrnConfig().getJSONObject("content_indexation");
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	private static JSONObject getContentIndexation() throws TrnConfigException {
+		try {
+			return getTrnConfig().getJSONObject("content_indexation");
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  public static String getIndexEngine() throws TrnConfigException {
-    try {
-      return getContentIndexation().getString("engine");
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	public static String getIndexEngine() throws TrnConfigException {
+		try {
+			return getContentIndexation().getString("engine");
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  public static JSONObject getEsiConfig() throws TrnConfigException {
-    try {
-      return getContentIndexation().getJSONObject("esi_config");
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	public static JSONObject getEsiConfig() throws TrnConfigException {
+		try {
+			return getContentIndexation().getJSONObject("esi_config");
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  private static JSONObject getGitConfig() throws TrnConfigException {
-    try {
-      return getContentEdition().getJSONObject("git_checkout_service");
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	private static JSONObject getGitConfig() throws TrnConfigException {
+		try {
+			return getContentEdition().getJSONObject("git_checkout_service");
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  public static String getGitUrl() throws TrnConfigException {
-    try {
-      return getGitConfig().getJSONObject("repository").getString("uri");
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	public static String getGitUrl() throws TrnConfigException {
+		try {
+			return getGitConfig().getJSONObject("repository").getString("uri");
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  public static String getGitBranch() throws TrnConfigException {
-    try {
-      return getGitConfig().getJSONObject("repository").getString("branch");
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	// return true if url is ssl url, false if other
+	public static boolean isGitUrlSSL() throws TrnConfigException {
+		try {
+			Matcher matcher = SSL_URL_PATTERN.matcher(getGitUrl());
+      		if (matcher.find()) {
+				AppLog.info("URL is SSL", Grant.getSystemAdmin());
+				return true;
+			} else {
+				AppLog.info("URL is not SSL", Grant.getSystemAdmin());
+				return false;
+			}
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  public static JSONObject getGitCredentials() throws TrnConfigException {
-    try {
-      return getGitConfig().getJSONObject("repository").getJSONObject("creds");
-    } catch (Exception e) {
-      throw new TrnConfigException(e.getMessage());
-    }
-  }
+	public static String getGitBranch() throws TrnConfigException {
+		try {
+			return getGitConfig().getJSONObject("repository").getString("branch");
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  public static String[] getLangs(Grant g) {
-    return g.getListOfValues("LANG_ALL").getCodesArray("LANG_ALL");
-  }
+	public static JSONObject getGitCredentials() throws TrnConfigException {
+		try {
+			return getGitConfig().getJSONObject("repository").getJSONObject("creds");
+		} catch (Exception e) {
+			throw new TrnConfigException(e.getMessage());
+		}
+	}
 
-  public static String[] getLangs(Grant g, boolean includeDefault) {
-    if (includeDefault)
-      return getLangs(g);
-    else {
-      List<String> langs = new LinkedList<String>(Arrays.asList(getLangs(g)));
-      langs.remove(getDefaultLang());
-      return langs.toArray(new String[0]);
-    }
-  }
+	public static String[] getLangs(Grant g) {
+		return g.getListOfValues("LANG_ALL").getCodesArray("LANG_ALL");
+	}
 
-  public static String getDefaultLang() {
-    return "ANY";
-  }
+	public static String[] getLangs(Grant g, boolean includeDefault) {
+		if (includeDefault)
+			return getLangs(g);
+		else {
+			List<String> langs = new LinkedList<String>(Arrays.asList(getLangs(g)));
+			langs.remove(getDefaultLang());
+			return langs.toArray(new String[0]);
+		}
+	}
+
+	public static String getDefaultLang() {
+		return "ANY";
+	}
 }
--- src/com/simplicite/extobjects/Training/TrnGitCheckoutService.java
+++ src/com/simplicite/extobjects/Training/TrnGitCheckoutService.java
@@ -9,15 +9,10 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.merge.ContentMergeStrategy;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.transport.RefSpec;
-import org.eclipse.jgit.transport.SshSessionFactory;
-import org.eclipse.jgit.transport.SshTransport;
-import org.eclipse.jgit.transport.Transport;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-import org.eclipse.jgit.transport.sshd.SshdSessionFactory;
 import org.eclipse.jgit.util.FileUtils;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.TransportConfigCallback;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.json.JSONObject;
 
@@ -33,160 +28,162 @@ import com.simplicite.util.tools.Parameters;
  * External object TrnGitCheckoutService
  */
 public class TrnGitCheckoutService extends com.simplicite.webapp.services.RESTServiceExternalObject {
-  private static final long serialVersionUID = 1L;
-  private static final String CONTENT_FOLDER_NAME = "docs";
-  private UsernamePasswordCredentialsProvider credentials;
+	private static final long serialVersionUID = 1L;
+	private static final String CONTENT_FOLDER_NAME = "docs";
+	// credentialsProvider are used only in a https scenario
+	// they will remain null if the url is in a ssl format
+	private UsernamePasswordCredentialsProvider credentialsProvider;
 
-  @Override
-  public Object post(Parameters params) {
-    try {
-      String branch = TrnTools.getGitBranch();
-      File contentDir = getContentDir();
-      String msg;
-      setUsernameTokenCredentials();
-      if (!contentDir.exists()) {
-        String gitUrl = TrnTools.getGitUrl();
-        performClone(gitUrl, branch, contentDir);
-        msg = "Result: " + gitUrl + " has been successfully cloned. Current branch: " + branch + ".";
-      } else {
-        performPull(branch, contentDir);
-        msg = "The content has been successfully updated. Current branch: " + branch + ".";
-      }
-      TrnFsSyncTool.triggerSync();
-      return msg;
-    } catch (IOException | GitAPIException | TrnConfigException | TrnSyncException e) {
-      setHTTPStatus(500);
-      AppLog.error(getClass(), "post", e.getMessage(), e, getGrant());
-      return "ERROR : " + e.getMessage();
-    }
-  }
+	@Override
+	public Object post(Parameters params) {
+		try {
+			String branch = TrnTools.getGitBranch();
+			File contentDir = getContentDir();
+			String msg;
+			setUsernameTokenCredentials();
+			if (!contentDir.exists()) {
+				String gitUrl = TrnTools.getGitUrl();
+				performClone(gitUrl, branch, contentDir);
+				msg = "Result: " + gitUrl + " has been successfully cloned. Current branch: " + branch + ".";
+			} else {
+				performPull(branch, contentDir);
+				msg = "The content has been successfully updated. Current branch: " + branch + ".";
+			}
+			TrnFsSyncTool.triggerSync();
+			return msg;
+		} catch (IOException | GitAPIException | TrnConfigException | TrnSyncException e) {
+			setHTTPStatus(500);
+			AppLog.error(getClass(), "post", e.getMessage(), e, getGrant());
+			return "ERROR : " + e.getMessage();
+		}
+	}
 
-  private void performClone(String remoteUrl, String branch, File contentDir) throws GitAPIException {
-    AppLog.info("Attempting to clone " + remoteUrl + " on branch " + branch, getGrant());
-    CloneCommand cloneCommand = Git.cloneRepository()
-        .setURI(remoteUrl)
-        .setDirectory(contentDir)
-        .setCloneAllBranches(false)
-        .setBranch(branch)
-        .setCloneSubmodules(false)
-        .setDepth(1)
-        .setCredentialsProvider(credentials);
+	private void performClone(String remoteUrl, String branch, File contentDir) throws GitAPIException {
+		AppLog.info("Attempting to clone " + remoteUrl + " on branch " + branch, getGrant());
+		CloneCommand cloneCommand = Git.cloneRepository()
+				.setURI(remoteUrl)
+				.setDirectory(contentDir)
+				.setCloneAllBranches(false)
+				.setBranch(branch)
+				.setCloneSubmodules(false)
+				.setDepth(1)
+				.setCredentialsProvider(credentialsProvider);
 
-    try (Git git = cloneCommand.call()) {
-      AppLog.info("Shallow clone completed successfully.", getGrant());
-    }
-  }
+		try (Git git = cloneCommand.call()) {
+			AppLog.info("Shallow clone completed successfully.", getGrant());
+		}
+	}
 
-  private void performPull(String branch, File contentDir) throws IOException, GitAPIException, TrnConfigException {
-    AppLog.info("Attempting to pull on branch " + branch, getGrant());
-    FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
-    Repository repository = repositoryBuilder.setGitDir(new File(contentDir, ".git"))
-        .readEnvironment()
-        .findGitDir()
-        .build();
+	private void performPull(String branch, File contentDir) throws IOException, GitAPIException, TrnConfigException {
+		AppLog.info("Attempting to pull on branch " + branch, getGrant());
+		FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
+		Repository repository = repositoryBuilder.setGitDir(new File(contentDir, ".git"))
+				.readEnvironment()
+				.findGitDir()
+				.build();
 
-    try (Git git = new Git(repository)) {
+		try (Git git = new Git(repository)) {
 
-      git.fetch()
-          .setRemote("origin")
-          // map all the local branches with their remote tracking branches
-          .setRefSpecs(new RefSpec("+refs/heads/*:refs/remotes/origin/*"))
-          .setCredentialsProvider(credentials)
-          .call();
-      AppLog.info("Successfully fetched from remote repository", getGrant());
+			git.fetch()
+					.setRemote("origin")
+					// map all the local branches with their remote tracking branches
+					.setRefSpecs(new RefSpec("+refs/heads/*:refs/remotes/origin/*"))
+					.setCredentialsProvider(credentialsProvider)
+					.call();
+			AppLog.info("Successfully fetched from remote repository", getGrant());
 
-      if (branchExistsLocally(git, branch)) {
-        // Switch to the existing local branch
-        git.checkout()
-            .setName(branch)
-            .call();
-        AppLog.info("Checkout on existing branch: " + branch, getGrant());
-      } else {
-        // Create a new branch from the remote branch
-        git.checkout()
-            .setCreateBranch(true)
-            .setName(branch)
-            .setStartPoint("origin/" + branch)
-            .call();
-        AppLog.info("Checkout on new branch: " + branch, getGrant());
-      }
+			if (branchExistsLocally(git, branch)) {
+				// Switch to the existing local branch
+				git.checkout()
+						.setName(branch)
+						.call();
+				AppLog.info("Checkout on existing branch: " + branch, getGrant());
+			} else {
+				// Create a new branch from the remote branch
+				git.checkout()
+						.setCreateBranch(true)
+						.setName(branch)
+						.setStartPoint("origin/" + branch)
+						.call();
+				AppLog.info("Checkout on new branch: " + branch, getGrant());
+			}
 
-      git.pull()
-          .setRemote("origin")
-          .setRemoteBranchName(branch)
-          .setCredentialsProvider(credentials)
-          .setContentMergeStrategy(ContentMergeStrategy.THEIRS)
-          .call();
-      AppLog.info("Remote content has been pulled", getGrant());
+			git.pull()
+					.setRemote("origin")
+					.setRemoteBranchName(branch)
+					.setCredentialsProvider(credentialsProvider)
+					.setContentMergeStrategy(ContentMergeStrategy.THEIRS)
+					.call();
+			AppLog.info("Remote content has been pulled", getGrant());
 
-      AppLog.info("Checkout done.", getGrant());
-    } catch (GitAPIException e) {
-      AppLog.warning(getClass(), "performPull",
-          "Unable to pull. The pull command fallback will try to delete and clone back the repository", e, getGrant());
-      pullCommandFallback(TrnTools.getGitUrl(), branch, contentDir);
-    }
-  }
+			AppLog.info("Checkout done.", getGrant());
+		} catch (GitAPIException e) {
+			AppLog.warning(getClass(), "performPull",
+					"Unable to pull. The pull command fallback will try to delete and clone back the repository", e,
+					getGrant());
+			pullCommandFallback(TrnTools.getGitUrl(), branch, contentDir);
+		}
+	}
 
-  private boolean branchExistsLocally(Git git, String branch) throws GitAPIException {
-    List<Ref> branches = git.branchList().call();
-    boolean branchExistsLocally = false;
-    for (Ref ref : branches) {
-      if (ref.getName().equals("refs/heads/" + branch)) {
-        branchExistsLocally = true;
-        break;
-      }
-    }
-    return branchExistsLocally;
-  }
+	private static boolean branchExistsLocally(Git git, String branch) throws GitAPIException {
+		List<Ref> branches = git.branchList().call();
+		boolean branchExistsLocally = false;
+		for (Ref ref : branches) {
+			if (ref.getName().equals("refs/heads/" + branch)) {
+				branchExistsLocally = true;
+				break;
+			}
+		}
+		return branchExistsLocally;
+	}
 
-  private void pullCommandFallback(String remoteUrl, String branch, File contentDir)
-      throws IOException, GitAPIException {
-    deleteRepository();
-    performClone(remoteUrl, branch, contentDir);
-    AppLog.info("The pull command fallback has deleted and cloned back the repository", getGrant());
-  }
+	private void pullCommandFallback(String remoteUrl, String branch, File contentDir)
+			throws IOException, GitAPIException {
+		deleteRepository();
+		performClone(remoteUrl, branch, contentDir);
+		AppLog.info("The pull command fallback has deleted and cloned back the repository", getGrant());
+	}
 
-  private void setUsernameTokenCredentials() throws TrnConfigException {
-    JSONObject jsonCreds = TrnTools.getGitCredentials();
-    credentials = new UsernamePasswordCredentialsProvider(
-    	jsonCreds.getString("username"),
-        jsonCreds.getString("token")
-    );
-    
-    // quickfix use ssh  
-    credentials = null; 
-	SshdSessionFactory sshSessionFactory = new org.eclipse.jgit.transport.sshd.SshdSessionFactoryBuilder()
-	        .setPreferredAuthentications("publickey")
-	        .setHomeDirectory(org.eclipse.jgit.util.FS.DETECTED.userHome())
-	        .setSshDirectory(new File(org.eclipse.jgit.util.FS.DETECTED.userHome(), "/.ssh"))
-	        .build(null);
-  }
+	private void setUsernameTokenCredentials() throws TrnConfigException {
+		if (TrnTools.isGitUrlSSL()) {
+			JSONObject jsonCreds = TrnTools.getGitCredentials();
+			credentialsProvider = new UsernamePasswordCredentialsProvider(
+					jsonCreds.getString("username"),
+					jsonCreds.getString("token"));
+		} else {
+			new org.eclipse.jgit.transport.sshd.SshdSessionFactoryBuilder()
+					.setPreferredAuthentications("publickey")
+					.setHomeDirectory(org.eclipse.jgit.util.FS.DETECTED.userHome())
+					.setSshDirectory(new File(org.eclipse.jgit.util.FS.DETECTED.userHome(), "/.ssh"))
+					.build(null);
+		}
+	}
 
-  @Override
-  public Object del(Parameters params) {
-    try {
-      return deleteRepository();
-    } catch (IOException e) {
-      return "ERROR : " + e.getMessage();
-    }
-  }
+	@Override
+	public Object del(Parameters params) {
+		try {
+			return deleteRepository();
+		} catch (IOException e) {
+			return "ERROR : " + e.getMessage();
+		}
+	}
 
-  private String deleteRepository() throws IOException {
-    File repositoryDir = getContentDir();
-    String msg;
-    if (repositoryDir.exists()) {
-      FileUtils.delete(repositoryDir, FileUtils.RECURSIVE);
-      msg = "Repository deleted successfully.";
-      AppLog.info(msg, getGrant());
-      return msg;
-    } else {
-      msg = "Repository directory does not exist.";
-      AppLog.info(msg, getGrant());
-      return msg;
-    }
-  }
+	private String deleteRepository() throws IOException {
+		File repositoryDir = getContentDir();
+		String msg;
+		if (repositoryDir.exists()) {
+			FileUtils.delete(repositoryDir, FileUtils.RECURSIVE);
+			msg = "Repository deleted successfully.";
+			AppLog.info(msg, getGrant());
+			return msg;
+		} else {
+			msg = "Repository directory does not exist.";
+			AppLog.info(msg, getGrant());
+			return msg;
+		}
+	}
 
-  private File getContentDir() {
-    return new File(Platform.getContentDir(), CONTENT_FOLDER_NAME);
-  }
+	private static File getContentDir() {
+		return new File(Platform.getContentDir(), CONTENT_FOLDER_NAME);
+	}
 }
