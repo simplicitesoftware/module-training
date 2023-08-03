22db23fd667e997e9a8b4e14f5ec340c00ed039e
designer (Designer) <no-reply@simplicite.fr>
2023-08-03 17:12:55 +0200

    add TrnTokenBucket code file

---
+++ src/com/simplicite/commons/Training/TrnTokenBucket.java
@@ -0,0 +1,17 @@
+package com.simplicite.commons.Training;
+
+import java.util.*;
+
+import com.simplicite.util.*;
+import com.simplicite.bpm.*;
+import com.simplicite.util.exceptions.*;
+import com.simplicite.util.tools.*;
+
+/**
+ * Shared code TrnTokenBucket
+ */
+public class TrnTokenBucket implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+
+	
+}
