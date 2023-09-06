52757dd88b3e1447826a4ae5d31de6618764da1e
designer (Designer) <no-reply@simplicite.fr>
2023-09-06 11:49:55 +0200

    create TrnIndexService

---
+++ src/com/simplicite/extobjects/Training/TrnIndexService.java
@@ -0,0 +1,14 @@
+package com.simplicite.extobjects.Training;
+
+import java.util.*;
+
+import com.simplicite.util.*;
+import com.simplicite.util.exceptions.*;
+import com.simplicite.util.tools.*;
+
+/**
+ * External object TrnIndexService
+ */
+public class TrnIndexService extends com.simplicite.webapp.services.RESTServiceExternalObject { 
+	
+}
