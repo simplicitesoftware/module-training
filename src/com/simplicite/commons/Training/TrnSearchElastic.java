f4c5645a0910cbfe8593464aeae1e7a22a9d4e75
designer (Designer) <no-reply@simplicite.fr>
2023-09-06 14:02:25 +0200

    create simplicite and elasticsearch files

---
+++ src/com/simplicite/commons/Training/TrnSearchElastic.java
@@ -0,0 +1,17 @@
+package com.simplicite.commons.Training;
+
+import java.util.*;
+
+import com.simplicite.util.*;
+import com.simplicite.bpm.*;
+import com.simplicite.util.exceptions.*;
+import com.simplicite.util.tools.*;
+
+/**
+ * Shared code TrnSearchElastic
+ */
+public class TrnSearchElastic implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+
+	
+}
---
+++ src/com/simplicite/commons/Training/TrnSearchSimplicite.java
@@ -0,0 +1,17 @@
+package com.simplicite.commons.Training;
+
+import java.util.*;
+
+import com.simplicite.util.*;
+import com.simplicite.bpm.*;
+import com.simplicite.util.exceptions.*;
+import com.simplicite.util.tools.*;
+
+/**
+ * Shared code TrnSearchSimplicite
+ */
+public class TrnSearchSimplicite implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+
+	
+}
