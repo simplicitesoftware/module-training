922e6355d94c02e393421c2b435ea3fb85d0ea5a
gaubert <germain40k@hotmail.fr>
2023-09-06 13:19:54 +0200

    organized GitCheckout

--- src/com/simplicite/commons/Training/TrnGitCheckout.java
+++ src/com/simplicite/commons/Training/TrnGitCheckout.java
@@ -1,17 +1,171 @@
 package com.simplicite.commons.Training;
 
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
-import com.simplicite.util.*;
-import com.simplicite.bpm.*;
-import com.simplicite.util.exceptions.*;
-import com.simplicite.util.tools.*;
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.merge.ContentMergeStrategy;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.eclipse.jgit.util.FileUtils;
+import org.json.JSONObject;
+
+import com.simplicite.util.AppLog;
+import com.simplicite.util.Grant;
+import com.simplicite.util.engine.Platform;
 
 /**
  * Shared code TrnGitCheckout
  */
 public class TrnGitCheckout implements java.io.Serializable {
 	private static final long serialVersionUID = 1L;
+    private static final String CONTENT_FOLDER_NAME = "docs";
+    private UsernamePasswordCredentialsProvider credentialsProvider;
+    private Grant g;
 
-	
+    public TrnGitCheckout(Grant g) {
+        this.g = g;
+    }
+
+	public String checkout() throws TrnConfigException, GitAPIException, IOException, TrnSyncException {
+        String branch = TrnTools.getGitBranch();
+        File contentDir = getContentDir();
+        String msg;
+        setAuthentication();
+        if (!contentDir.exists()) {
+            String gitUrl = TrnTools.getGitUrl();
+            performClone(gitUrl, branch, contentDir);
+            msg = "Result: " + gitUrl + " has been successfully cloned. Current branch: " + branch + ".";
+        } else {
+            performPull(branch, contentDir);
+            msg = "The content has been successfully updated. Current branch: " + branch + ".";
+        }
+        TrnFsSyncTool.triggerSync();
+        return msg;
+    }
+
+    private void performClone(String remoteUrl, String branch, File contentDir) throws GitAPIException {
+		AppLog.info("Attempting to clone " + remoteUrl + " on branch " + branch, g);
+		CloneCommand cloneCommand = Git.cloneRepository()
+				.setURI(remoteUrl)
+				.setDirectory(contentDir)
+				.setCloneAllBranches(false)
+				.setBranch(branch)
+				.setCloneSubmodules(false)
+				.setDepth(1)
+				.setCredentialsProvider(credentialsProvider);
+
+		try (Git git = cloneCommand.call()) {
+			AppLog.info("Shallow clone completed successfully.", g);
+		}
+	}
+
+	private void performPull(String branch, File contentDir) throws IOException, GitAPIException, TrnConfigException {
+		AppLog.info("Attempting to pull on branch " + branch, g);
+		FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
+		Repository repository = repositoryBuilder.setGitDir(new File(contentDir, ".git"))
+				.readEnvironment()
+				.findGitDir()
+				.build();
+
+		try (Git git = new Git(repository)) {
+
+			git.fetch()
+					.setRemote("origin")
+					// map all the local branches with their remote tracking branches
+					.setRefSpecs(new RefSpec("+refs/heads/*:refs/remotes/origin/*"))
+					.setCredentialsProvider(credentialsProvider)
+					.call();
+			AppLog.info("Successfully fetched from remote repository", g);
+
+			if (branchExistsLocally(git, branch)) {
+				// Switch to the existing local branch
+				git.checkout()
+						.setName(branch)
+						.call();
+				AppLog.info("Checkout on existing branch: " + branch, g);
+			} else {
+				// Create a new branch from the remote branch
+				git.checkout()
+						.setCreateBranch(true)
+						.setName(branch)
+						.setStartPoint("origin/" + branch)
+						.call();
+				AppLog.info("Checkout on new branch: " + branch, g);
+			}
+
+			git.pull()
+					.setRemote("origin")
+					.setRemoteBranchName(branch)
+					.setCredentialsProvider(credentialsProvider)
+					.setContentMergeStrategy(ContentMergeStrategy.THEIRS)
+					.call();
+			AppLog.info("Remote content has been pulled", g);
+
+			AppLog.info("Checkout done.", g);
+		} catch (GitAPIException e) {
+			AppLog.warning("Unable to pull. The pull command fallback will try to delete and clone back the repository", e, g);
+			pullCommandFallback(TrnTools.getGitUrl(), branch, contentDir);
+		}
+	}
+
+	private static boolean branchExistsLocally(Git git, String branch) throws GitAPIException {
+		List<Ref> branches = git.branchList().call();
+		boolean branchExistsLocally = false;
+		for (Ref ref : branches) {
+			if (ref.getName().equals("refs/heads/" + branch)) {
+				branchExistsLocally = true;
+				break;
+			}
+		}
+		return branchExistsLocally;
+	}
+
+	private void pullCommandFallback(String remoteUrl, String branch, File contentDir)
+			throws IOException, GitAPIException {
+		deleteRepository();
+		performClone(remoteUrl, branch, contentDir);
+		AppLog.info("The pull command fallback has deleted and cloned back the repository", g);
+	}
+
+	private void setAuthentication() throws TrnConfigException {
+		if (TrnTools.isGitUrlSSH()) {
+			credentialsProvider = null;
+			new org.eclipse.jgit.transport.sshd.SshdSessionFactoryBuilder()
+					.setPreferredAuthentications("publickey")
+					.setHomeDirectory(org.eclipse.jgit.util.FS.DETECTED.userHome())
+					.setSshDirectory(new File(org.eclipse.jgit.util.FS.DETECTED.userHome(), "/.ssh"))
+					.build(null);
+		} else {
+			JSONObject jsonCreds = TrnTools.getGitCredentials();
+			credentialsProvider = new UsernamePasswordCredentialsProvider(
+					jsonCreds.getString("username"),
+					jsonCreds.getString("token"));
+		}
+	}
+
+	public String deleteRepository() throws IOException {
+		File repositoryDir = getContentDir();
+		String msg;
+		if (repositoryDir.exists()) {
+			FileUtils.delete(repositoryDir, FileUtils.RECURSIVE);
+			msg = "Repository deleted successfully.";
+			AppLog.info(msg, g);
+			return msg;
+		} else {
+			msg = "Repository directory does not exist.";
+			AppLog.info(msg, g);
+			return msg;
+		}
+	}
+
+    private static File getContentDir() {
+		return new File(Platform.getContentDir(), CONTENT_FOLDER_NAME);
+	}
 }
--- src/com/simplicite/extobjects/Training/TrnGitCheckoutService.java
+++ src/com/simplicite/extobjects/Training/TrnGitCheckoutService.java
@@ -1,27 +1,13 @@
 package com.simplicite.extobjects.Training;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.merge.ContentMergeStrategy;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.eclipse.jgit.transport.RefSpec;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-import org.eclipse.jgit.util.FileUtils;
-import org.eclipse.jgit.api.CloneCommand;
-import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.json.JSONObject;
 
 import com.simplicite.commons.Training.TrnConfigException;
-import com.simplicite.commons.Training.TrnFsSyncTool;
+import com.simplicite.commons.Training.TrnGitCheckout;
 import com.simplicite.commons.Training.TrnSyncException;
-import com.simplicite.commons.Training.TrnTools;
-import com.simplicite.util.*;
-import com.simplicite.util.engine.Platform;
+import com.simplicite.util.AppLog;
 import com.simplicite.util.tools.Parameters;
 
 /**
@@ -29,28 +15,15 @@ import com.simplicite.util.tools.Parameters;
  */
 public class TrnGitCheckoutService extends com.simplicite.webapp.services.RESTServiceExternalObject {
 	private static final long serialVersionUID = 1L;
-	private static final String CONTENT_FOLDER_NAME = "docs";
+	
 	// credentialsProvider are used only in a https scenario
 	// they will remain null if the url is in a ssh format
-	private UsernamePasswordCredentialsProvider credentialsProvider;
 
 	@Override
 	public Object post(Parameters params) {
 		try {
-			String branch = TrnTools.getGitBranch();
-			File contentDir = getContentDir();
-			String msg;
-			setAuthentication();
-			if (!contentDir.exists()) {
-				String gitUrl = TrnTools.getGitUrl();
-				performClone(gitUrl, branch, contentDir);
-				msg = "Result: " + gitUrl + " has been successfully cloned. Current branch: " + branch + ".";
-			} else {
-				performPull(branch, contentDir);
-				msg = "The content has been successfully updated. Current branch: " + branch + ".";
-			}
-			TrnFsSyncTool.triggerSync();
-			return msg;
+            TrnGitCheckout tgc = new TrnGitCheckout(getGrant());
+			return tgc.checkout();
 		} catch (IOException | GitAPIException | TrnConfigException | TrnSyncException e) {
 			setHTTPStatus(500);
 			AppLog.error(getClass(), "post", e.getMessage(), e, getGrant());
@@ -58,133 +31,13 @@ public class TrnGitCheckoutService extends com.simplicite.webapp.services.RESTSe
 		}
 	}
 
-	private void performClone(String remoteUrl, String branch, File contentDir) throws GitAPIException {
-		AppLog.info("Attempting to clone " + remoteUrl + " on branch " + branch, getGrant());
-		CloneCommand cloneCommand = Git.cloneRepository()
-				.setURI(remoteUrl)
-				.setDirectory(contentDir)
-				.setCloneAllBranches(false)
-				.setBranch(branch)
-				.setCloneSubmodules(false)
-				.setDepth(1)
-				.setCredentialsProvider(credentialsProvider);
-
-		try (Git git = cloneCommand.call()) {
-			AppLog.info("Shallow clone completed successfully.", getGrant());
-		}
-	}
-
-	private void performPull(String branch, File contentDir) throws IOException, GitAPIException, TrnConfigException {
-		AppLog.info("Attempting to pull on branch " + branch, getGrant());
-		FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
-		Repository repository = repositoryBuilder.setGitDir(new File(contentDir, ".git"))
-				.readEnvironment()
-				.findGitDir()
-				.build();
-
-		try (Git git = new Git(repository)) {
-
-			git.fetch()
-					.setRemote("origin")
-					// map all the local branches with their remote tracking branches
-					.setRefSpecs(new RefSpec("+refs/heads/*:refs/remotes/origin/*"))
-					.setCredentialsProvider(credentialsProvider)
-					.call();
-			AppLog.info("Successfully fetched from remote repository", getGrant());
-
-			if (branchExistsLocally(git, branch)) {
-				// Switch to the existing local branch
-				git.checkout()
-						.setName(branch)
-						.call();
-				AppLog.info("Checkout on existing branch: " + branch, getGrant());
-			} else {
-				// Create a new branch from the remote branch
-				git.checkout()
-						.setCreateBranch(true)
-						.setName(branch)
-						.setStartPoint("origin/" + branch)
-						.call();
-				AppLog.info("Checkout on new branch: " + branch, getGrant());
-			}
-
-			git.pull()
-					.setRemote("origin")
-					.setRemoteBranchName(branch)
-					.setCredentialsProvider(credentialsProvider)
-					.setContentMergeStrategy(ContentMergeStrategy.THEIRS)
-					.call();
-			AppLog.info("Remote content has been pulled", getGrant());
-
-			AppLog.info("Checkout done.", getGrant());
-		} catch (GitAPIException e) {
-			AppLog.warning(getClass(), "performPull",
-					"Unable to pull. The pull command fallback will try to delete and clone back the repository", e,
-					getGrant());
-			pullCommandFallback(TrnTools.getGitUrl(), branch, contentDir);
-		}
-	}
-
-	private static boolean branchExistsLocally(Git git, String branch) throws GitAPIException {
-		List<Ref> branches = git.branchList().call();
-		boolean branchExistsLocally = false;
-		for (Ref ref : branches) {
-			if (ref.getName().equals("refs/heads/" + branch)) {
-				branchExistsLocally = true;
-				break;
-			}
-		}
-		return branchExistsLocally;
-	}
-
-	private void pullCommandFallback(String remoteUrl, String branch, File contentDir)
-			throws IOException, GitAPIException {
-		deleteRepository();
-		performClone(remoteUrl, branch, contentDir);
-		AppLog.info("The pull command fallback has deleted and cloned back the repository", getGrant());
-	}
-
-	private void setAuthentication() throws TrnConfigException {
-		if (TrnTools.isGitUrlSSH()) {
-			credentialsProvider = null;
-			new org.eclipse.jgit.transport.sshd.SshdSessionFactoryBuilder()
-					.setPreferredAuthentications("publickey")
-					.setHomeDirectory(org.eclipse.jgit.util.FS.DETECTED.userHome())
-					.setSshDirectory(new File(org.eclipse.jgit.util.FS.DETECTED.userHome(), "/.ssh"))
-					.build(null);
-		} else {
-			JSONObject jsonCreds = TrnTools.getGitCredentials();
-			credentialsProvider = new UsernamePasswordCredentialsProvider(
-					jsonCreds.getString("username"),
-					jsonCreds.getString("token"));
-		}
-	}
-
-	@Override
+    @Override
 	public Object del(Parameters params) {
 		try {
-			return deleteRepository();
+            TrnGitCheckout tgc = new TrnGitCheckout(getGrant());
+			return tgc.deleteRepository();
 		} catch (IOException e) {
 			return "ERROR : " + e.getMessage();
 		}
-	}
-
-	private String deleteRepository() throws IOException {
-		File repositoryDir = getContentDir();
-		String msg;
-		if (repositoryDir.exists()) {
-			FileUtils.delete(repositoryDir, FileUtils.RECURSIVE);
-			msg = "Repository deleted successfully.";
-			AppLog.info(msg, getGrant());
-			return msg;
-		} else {
-			msg = "Repository directory does not exist.";
-			AppLog.info(msg, getGrant());
-			return msg;
-		}
-	}
-
-	private static File getContentDir() {
-		return new File(Platform.getContentDir(), CONTENT_FOLDER_NAME);
 	}
 }
