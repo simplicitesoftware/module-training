0ee2c0ee7aa4013a77735f345f1e9be4f34e0922
designer (Designer) <no-reply@simplicite.fr>
2023-11-03 19:44:14 +0100

    removed useless helper class

--- src/com/simplicite/extobjects/Training/TrnSearchService.java
+++ src/com/simplicite/extobjects/Training/TrnSearchService.java
@@ -1,6 +1,8 @@
 package com.simplicite.extobjects.Training;
 
 import com.simplicite.commons.Training.TrnSearch;
+import com.simplicite.commons.Training.TrnSearchElastic;
+import com.simplicite.commons.Training.TrnSearchSimplicite;
 import com.simplicite.commons.Training.TrnTools;
 import com.simplicite.util.AppLog;
 import com.simplicite.util.Grant;
@@ -15,12 +17,23 @@ public class TrnSearchService extends RESTServiceExternalObject {
 	public Object get(Parameters params) {
         Grant g = getGrant();
         try {
-            return TrnSearch.search(
-            	TrnTools.getIndexEngine(), 
-            	params.getParameter("query"), 
-            	params.getParameter("lang"),
-            	g
-            );
+            String indexEngine = TrnTools.getIndexEngine();
+            String query = params.getParameter("query");
+            String lang = params.getParameter("lang");
+            
+            if("elasticsearch".equals(indexEngine)){
+                try {
+                    return TrnSearchElastic.search(query, lang, g);
+                }
+                catch(Exception e) {
+                    AppLog.warning("Error, unable to retrieve elasticsearch search results. Fallback to simplicite engine.", e, g);
+                }
+            }
+            else if(!"simplicite".equals(indexEngine)) {
+                AppLog.warning("Unkown index engine: "+indexEngine+" found in TRN_CONFIG. Fallback to simplicite engine.", null, g);
+            }
+            
+            return TrnSearchSimplicite.search(query, lang, g);
         } catch(Exception e) {
             AppLog.error(getClass(), "get", e.getMessage(), e, g);
             setHTTPStatus(500);
