13c585e9e72f908f23233d2f9d70b71145d5d709
designer (Designer) <no-reply@simplicite.fr>
2023-11-22 17:31:17 +0100

    updated java version

---
+++ src/com/simplicite/objects/Training/TrnCategoryTranslate.java
@@ -0,0 +1,12 @@
+package com.simplicite.objects.Training;
+
+import com.simplicite.commons.Training.*;
+
+/**
+ * Business object TrnCategoryTranslate
+ */
+public class TrnCategoryTranslate extends TrnObject {
+	private static final long serialVersionUID = 1L;
+	
+	
+}
