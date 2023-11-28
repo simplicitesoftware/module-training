6d9f7a16908f5efaa2539c48d9d60cdf27026e20
gaubert <germain40k@hotmail.fr>
2023-11-28 14:27:34 +0100

    theme extends TrnObject

--- src/com/simplicite/objects/Training/TrnSiteTheme.java
+++ src/com/simplicite/objects/Training/TrnSiteTheme.java
@@ -1,14 +1,11 @@
 package com.simplicite.objects.Training;
 
-import java.util.*;
-
+import com.simplicite.commons.Training.TrnObject;
 import com.simplicite.util.*;
-import com.simplicite.util.exceptions.*;
-import com.simplicite.util.tools.*;
 
 /**
  * Business object TrnSiteTheme
  */
-public class TrnSiteTheme extends ObjectDB {
+public class TrnSiteTheme extends TrnObject {
 	private static final long serialVersionUID = 1L;
 }
