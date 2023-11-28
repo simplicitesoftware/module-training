4dce167ed184cafb24482b6ae9d66c709bf6da3d
designer (Designer) <no-reply@simplicite.fr>
2023-11-28 14:25:39 +0100

    add theme java file

---
+++ src/com/simplicite/objects/Training/TrnSiteTheme.java
@@ -0,0 +1,14 @@
+package com.simplicite.objects.Training;
+
+import java.util.*;
+
+import com.simplicite.util.*;
+import com.simplicite.util.exceptions.*;
+import com.simplicite.util.tools.*;
+
+/**
+ * Business object TrnSiteTheme
+ */
+public class TrnSiteTheme extends ObjectDB {
+	private static final long serialVersionUID = 1L;
+}
